a439a54c12cdb9a97006c87b07d065ff67901c55
Watanabe Shinnosuke <snnskwtnb@gmail.com>
2019-05-30 19:41:56 +0900

    add error codes

--- index.js
+++ index.js
@@ -6,8 +6,20 @@ const contents = ['⠀', '⣀', '⣤', '⣶', '⣿'];
 const MAX = contents.length - 1;
 
 module.exports = function verticalMeter(...args) {
-	if (args.length !== 1) {
-		throw new RangeError(`Expected 1 argument (<number>), but got ${args.length || 'no'} arguments.`);
+	const argLen = args.length;
+
+	if (argLen === 0) {
+		const error = new RangeError('Expected 1 argument (<number>), but got no arguments.');
+
+		error.code = 'ERR_MISSING_ARGS';
+		throw error;
+	}
+
+	if (argLen !== 1) {
+		const error = new RangeError(`Expected 1 argument (<number>), but got ${argLen} arguments.`);
+
+		error.code = 'ERR_TOO_MANY_ARGS';
+		throw error;
 	}
 
 	return contents[Math.round(rateMap(args[0], 0, MAX))];
